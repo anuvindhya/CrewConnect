16eb49829d0394545068779d864990fc300b696f
anuvindhya <anuvindhyasweety4332@gmail.com>
2026-09-05 19:23:03 +0530

    Update email configuration in settings.py

--- CrewConnect/settings.py
+++ CrewConnect/settings.py
@@ -129,6 +129,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'anuvindhyasweety4332@gmail.com'
+EMAIL_HOST_USER = 'your_email_id'
 EMAIL_HOST_PASSWORD = 'your_new_app_password'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
